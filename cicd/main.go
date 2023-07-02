e4badea77c2cfad8dc42c77fe063e1f12a6a1d9c
oscar <oscarsjlh@gmail.com>
2023-07-02 11:30:11 +0100

    fix action

--- cicd/main.go
+++ cicd/main.go
@@ -31,7 +31,8 @@ func build(ctx context.Context) error {
 	Key := client.SetSecret("awsKey", os.Getenv("AWS_ACCESS_KEY_ID"))
 	Secret := client.SetSecret("awsSecret", os.Getenv("AWS_SECRET_ACCESS_KEY"))
 	awsRegion := client.SetSecret("awsRegion", os.Getenv("AWS_REGION"))
-	src := client.Host().Directory("../Web/site")
+	dir := client.SetSecret("DIR", os.Getenv("DIR"))
+	src := client.Host().Directory("$DIR/Web/site")
 	hugo := client.Container().
 		From("477601539816.dkr.ecr.eu-west-2.amazonaws.com/hugo-oscar-eu:latest")
 	hugo = hugo.WithDirectory(".", src).WithWorkdir(".")
@@ -45,6 +46,7 @@ func build(ctx context.Context) error {
 	hugo = hugo.WithSecretVariable("AWS_ACCESS_KEY_ID", Key)
 	hugo = hugo.WithSecretVariable("AWS_SECRET_ACCESS_KEY", Secret)
 	hugo = hugo.WithSecretVariable("AWS_DEFAULT_REGION", awsRegion)
+	hugo = hugo.WithSecretVariable("DIR", dir)
 	hugo = hugo.WithExec([]string{"hugo"})
 	hugo = hugo.WithExec([]string{"hugo", "deploy"})
 	output := hugo.Directory(export)
@@ -56,7 +58,7 @@ func build(ctx context.Context) error {
 }
 
 func auth() string {
-	cmd, err := exec.Command("/bin/sh", "./auth.sh").Output()
+	cmd, err := exec.Command("/bin/sh", "$DIR/cicd/auth.sh").Output()
 	if err != nil {
 		fmt.Printf("error %s", err)
 	}
